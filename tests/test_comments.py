8409e48cf748791bf834476bbeffee18d30ad667
HullaBulla <hullabulla666@gmail.com>
2024-05-08 18:22:26 +0200

    Squashed commit of the following:
    commit 659f283a9ff2903ae1cdcea796aa3f63702f7bf2
    Author: HullaBulla <fredrikbloms@gmail.com>
    Date:   Wed May 8 18:11:29 2024 +0200
    
        goodi
    and bumb version
    commit 048e2ee665e685584a06aee2c06297de0399d458
    Author: HullaBulla <fredrikbloms@gmail.com>
    Date:   Wed May 8 17:07:46 2024 +0200
    
        test works

---
+++ tests/test_comments.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from click.testing import CliRunner
+
+from yt import cli
+
+
+def _read_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = f.read()
+    return data
+
+
+def _filepaths_textfiles():
+    return [
+        Path("tests/testdata/Video - rust game launcher.txt"),
+    ]
+
+
+def _filepaths_textfiles_expected():
+    return [
+        Path("tests/testdata/expected/Video - rust game launcher_expected_comments"),
+    ]
+
+
+def _load_files():
+    files = _filepaths_textfiles()
+    file_data = [_read_file(x) for x in files]
+    return file_data
+
+
+def test_comments():
+    expected_output = _read_file(_filepaths_textfiles_expected()[0])
+
+    files = _filepaths_textfiles()
+    file = files[0]
+    runner = CliRunner()
+    result = runner.invoke(cli, ["comments", str(file)])
+    print(result.stdout)
+
+    assert result.exit_code == 0
+    #assert result.output == expected_output
